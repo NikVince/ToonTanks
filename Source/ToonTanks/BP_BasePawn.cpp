6640c4b0485711a2cd86364c8823586565a8cacc
Nikolas (Daniel) Vincenti <165183152+darkstardust98@users.noreply.github.com>
2025-04-04 20:58:28 +0200

    UPDATED BP_BasePawn.h and BP_BasePawn.cpp with basic boilerplate for UCapsuleComponent implementation.

--- Source/ToonTanks/BP_BasePawn.cpp
+++ Source/ToonTanks/BP_BasePawn.cpp
@@ -2,13 +2,14 @@
 
 
 #include "BP_BasePawn.h"
+#include "Components/CapsuleComponent.h"
 
 // Sets default values
 ABP_BasePawn::ABP_BasePawn()
 {
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-	
+
 }
 
 // Called when the game starts or when spawned
