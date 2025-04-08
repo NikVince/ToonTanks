f58f34016bec465dced4a08e257305c51bfbb2ab
Nikolas (Daniel) Vincenti <165183152+NikVince@users.noreply.github.com>
2025-04-08 18:33:34 +0200

    UPDATED Tower.cpp with pseudo code for Tick() function and boilerplate for BeginPlay() function.

--- Source/ToonTanks/Tower.cpp
+++ Source/ToonTanks/Tower.cpp
@@ -7,5 +7,15 @@ void ATower::Tick(float DeltaTime)
 {
     Super::Tick(DeltaTime);
 
+    // Find the distance to the Tank
+
+    // Check if the Tank is in range
+
+    // If in range, rotate towards the Tank
+}
+
+void ATower::BeginPlay()
+{
+    Super::BeginPlay();
     
 }
