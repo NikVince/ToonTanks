b116177f5d36e67d4afdc979341d582848c50afa
Nikolas (Daniel) Vincenti <165183152+NikVince@users.noreply.github.com>
2025-04-08 18:25:56 +0200

    CREATED Tower.h/.cpp class based on BP_BasePawn to implement enemy Towers.

---
+++ Source/ToonTanks/Tower.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BP_BasePawn.h"
+#include "Tower.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TOONTANKS_API ATower : public ABP_BasePawn
+{
+	GENERATED_BODY()
+	
+};
