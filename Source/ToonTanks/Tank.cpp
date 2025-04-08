bb0441e5d0c7135dd3cb0dc26b6e30af8a36c94a
Nikolas (Daniel) Vincenti <165183152+NikVince@users.noreply.github.com>
2025-04-08 18:03:11 +0200

    UPDATED Tank.cpp to add a toggleable DebugSphere to see cursor for aiming the turret rotation.

--- Source/ToonTanks/Tank.cpp
+++ Source/ToonTanks/Tank.cpp
@@ -40,7 +40,7 @@ void ATank::Tick(float DeltaTime)
             ECollisionChannel::ECC_Visibility,
             false,
             HitResult);
-
+        //DrawDebugSphere(GetWorld(), HitResult.ImpactPoint, 25.f, 12, FColor::Red, false, -1.f);
         Super::RotateTurret(HitResult.ImpactPoint);
 
     }
