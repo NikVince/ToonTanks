439e32cf8b892bd2252d34994407a61cb1f219b2
Nikolas (Daniel) Vincenti <165183152+NikVince@users.noreply.github.com>
2025-04-10 18:30:34 +0200

    UPDATED Projectile.h/.cpp to implement Static Mesh Component.

--- Source/ToonTanks/Projectile.cpp
+++ Source/ToonTanks/Projectile.cpp
@@ -9,6 +9,9 @@ AProjectile::AProjectile()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	ProjectileMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Projectile Mesh"));
+	RootComponent = ProjectileMesh;
+
 }
 
 // Called when the game starts or when spawned
